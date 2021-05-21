ac0a8c61cb1a9670eb82befe7e1797d89d24c3f2
ardasar94 <sarihanarda@gmail.com>
2021-05-21 13:03:16 +0300

    Add project files.

---
+++ TodoApp/Controllers/OrnekController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Controllers
+{
+    [Route("BirkacSehir")]
+    [ApiController]
+    public class OrnekController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            return new[] { "Ankara", "İzmir", "Kars" };
+        }
+    }
+}
